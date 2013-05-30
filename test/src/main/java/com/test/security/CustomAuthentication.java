563297421463cdc0254a1df41daea6ba6cf926c6
unknown <cts1@LT016538.cts.com>
2013-05-30 11:59:59 +0530

    Implemented the Role based security login

---
+++ test/src/main/java/com/test/security/CustomAuthentication.java
@@ -0,0 +1,73 @@
+package com.test.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+public class CustomAuthentication implements Authentication {
+	
+	String name;
+    ArrayList<GrantedAuthority> authorities;
+    Object credentials;
+    Object details;
+    Object principal;
+    boolean authenticated;
+
+	public CustomAuthentication(String name, ArrayList<GrantedAuthority> authorities,
+			Object credentials, Object details, Object principal,
+			boolean authenticated) {
+		this.name = name;
+		this.authorities = authorities;
+		this.details = details;
+		this.principal = principal;
+		this.authenticated = authenticated;
+
+	}
+
+	
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		
+		return authorities;
+	}
+
+	@Override
+	public Object getCredentials() {
+		// TODO Auto-generated method stub
+		return principal;
+	}
+
+	@Override
+	public Object getDetails() {
+		// TODO Auto-generated method stub
+		return details;
+	}
+
+	@Override
+	public Object getPrincipal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAuthenticated() {
+		// TODO Auto-generated method stub
+		return authenticated;
+	}
+
+	@Override
+	public void setAuthenticated(boolean authenticated) throws IllegalArgumentException {
+		this.authenticated = authenticated;
+
+	}
+
+}
--- test/src/main/java/com/test/security/CustomAuthenticationProvider.java
+++ test/src/main/java/com/test/security/CustomAuthenticationProvider.java
@@ -1,14 +1,19 @@
 package com.test.security;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.stereotype.Component;
 
 import com.test.service.ILoginService;
 
+@SuppressWarnings("deprecation")
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
@@ -19,15 +24,23 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
 	@Override
 	public Authentication authenticate(Authentication authentication)throws AuthenticationException {
+		ArrayList<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		String username = authentication.getName();
+		String password = authentication.getCredentials().toString();
 		try {
-			boolean result = loginService.isValidUser(authentication.getName(), authentication.getCredentials().toString());
-			if (!result) {
-				return null;
+			boolean result = loginService.isValidUser(username, password);
+			if (result) {
+				authorities.add(new GrantedAuthorityImpl("ROLE_USER"));
+				return new CustomAuthentication(username, authorities, password, null, null, true);
+			}
+			else if("admin".equals(username) && "admin".equals(password)){
+				authorities.add(new GrantedAuthorityImpl("ROLE_ADMIN"));
+				return new CustomAuthentication(username, authorities, password, null, null, true);
 			}
 		} catch (Exception e) {
 			// TODO - Log the exception into log file.
 		}
-		return authentication;
+		return null;
 	}
 
 	@Override
