cca30b85af34456a97d2cc0b35d846a62e5a3d93
satishmotwani-cts <satish.motwani2@cognizant.com>
2013-06-10 13:48:13 +0530

    removed unneeded file
    prettified web xml

--- test/src/main/java/com/test/config/ProfileInitializer.java
+++ test/src/main/java/com/test/config/ProfileInitializer.java
@@ -9,7 +9,6 @@ import org.springframework.web.context.ConfigurableWebApplicationContext;
 /**
  * This is custom application initializer used by the application.
  * reference to this is specified in web.xml file.
- * 
  * In this implementation this custom initializer is used to set a profile using system environment variable.
  * Based on the value specified in environment variable, the specified profile is loaded. 
  *
