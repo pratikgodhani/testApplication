561c2f078268eeda5c9641e15fc8983edd08b205
satishmotwani-cts <satish.motwani2@cognizant.com>
2013-06-10 12:29:03 +0530

    added more comments

--- test/src/main/java/com/test/config/AppConfiguration.java
+++ test/src/main/java/com/test/config/AppConfiguration.java
@@ -20,6 +20,39 @@ import org.springframework.stereotype.Controller;
 
 import com.mongodb.MongoClient;
 
+/*
+ * Java Web Application web.xml applicationcontext context-param (s)
+ * contextClass = AnnotationConfigWebApplicationContext
+ * contextConfigLocation = com.test.config.AppConfiguration
+ * -----------------------------------------------------------
+ * The exact type of ConfigurableWebApplicationContext is specified as 
+ * AnnotationConfigWebApplicationContext since we are specifying our configuration 
+ * via Annotations, and not ApplicationContext.xml
+ * com.test.config.AppConfiguration is the location where to look for configuration
+ */ 
+
+/* 
+ * @Configuration indicates Spring that this is configuration.
+ * @ComponentScan annotation parameters tell Spring which are the base packages to 
+ * scan for configuration and the exclusion filters for component scan. 
+ * 
+ * To exclude an assignable type (e.g. to exclude a particular class), we specify 
+ * @Filter(value=MvcConfiguration.class, type=FilterType.ASSIGNABLE_TYPE)
+ * tells spring to not scan this component MvcConfiguration.class of type ASSIGNABLE_TYPE
+ * 
+ * Default value of type in Filter is AnnotationFilter. No need to specify FilterType in this case.
+ * So we say @Filter(value=Controller.class) to exclude all components that are annotated @Controller.
+ * 
+ * Note that in Spring, Controller itself is annotated as Component. 
+ * So, in a way, a Controller is also a Component.
+ * 
+ * @ImportResource - ??? 
+ * 
+ * @PropertySource({"registration.properties", "database.properties"}) tells Spring to load these 
+ * properties files into Environment. The properties files must be present in src/main/resources folder.
+ * In the war file, the files inside src/main/resources go in WEB-INF/classes folder.
+ */
+
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan(basePackages = "com.test", 
